db50434b35f46d10bc0742909ec2a18c734cd438
gissily <gissily@outlook.com>
2025-05-12 14:14:05 +0800

    update to redis 8.0

--- ci/script/latest_image.sh
+++ ci/script/latest_image.sh
@@ -4,5 +4,5 @@ docker pull mysql:8.0
 docker pull postgres:17
 docker pull apache/kafka:latest
 docker pull rabbitmq:management
-docker pull redis:7.2
+docker pull redis:8.0
 docker pull valkey/valkey:8.1
