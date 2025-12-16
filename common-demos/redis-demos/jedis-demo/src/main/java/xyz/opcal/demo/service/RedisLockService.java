d35b6235771a3e21a5c441fd2587d435b49902a1
gissily <gissily@outlook.com>
2025-12-16 22:32:20 +0800

    fix lock demo

--- common-demos/redis-demos/jedis-demo/src/main/java/xyz/opcal/demo/service/RedisLockService.java
+++ common-demos/redis-demos/jedis-demo/src/main/java/xyz/opcal/demo/service/RedisLockService.java
@@ -1,7 +1,7 @@
 package xyz.opcal.demo.service;
 
+import java.time.Duration;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.integration.redis.util.RedisLockRegistry;
@@ -15,7 +15,6 @@ public class RedisLockService {
 
 	private final RedisLockRegistry redisLockRegistry;
 
-	private final AtomicLong tryCounter = new AtomicLong();
 	private final AtomicLong counter = new AtomicLong();
 
 	public RedisLockService(RedisLockRegistry redisLockRegistry) {
@@ -24,21 +23,12 @@ public class RedisLockService {
 
 	public String get(String key) {
 		var lock = redisLockRegistry.obtain(key);
-		var isLock = false;
 		try {
-			var tryNum = tryCounter.incrementAndGet();
-			log.info("try Lock key: {}, count {}", key, tryNum);
-			isLock = lock.tryLock(10, TimeUnit.MILLISECONDS);
-			if (isLock) {
-				log.info("Lock key: {}, try {} count {}", key, tryNum, counter.incrementAndGet());
-			}
+			lock.lock(Duration.ofMillis(30));
+			log.info("Lock key: {}, count {}", key, counter.incrementAndGet());
 			return UUID.fromString(key).toString();
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
 		} finally {
-			if (isLock) {
-				lock.unlock();
-			}
+			lock.unlock();
 		}
 	}
 
