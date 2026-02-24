8070cc652fa928d591ee4732c030fd6fcf075b37
gissily <gissily@outlook.com>
2026-02-24 15:45:51 +0800

    update redis to 8.6

--- ci/script/latest_image.sh
+++ ci/script/latest_image.sh
@@ -4,5 +4,5 @@ docker pull mysql:8.0
 docker pull postgres:17
 docker pull apache/kafka:latest
 docker pull rabbitmq:management
-docker pull redis:8.4
+docker pull redis:8.6
 docker pull valkey/valkey:9.0
